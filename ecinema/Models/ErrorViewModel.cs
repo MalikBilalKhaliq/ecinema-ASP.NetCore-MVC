d8a003759e7ef10f92933dbaf1766c8ce39ecadc
MalikBilalKhaliq <bilalone7@gmail.com>
2022-10-29 13:10:58 +0200

    Add project files.

---
+++ ecinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
